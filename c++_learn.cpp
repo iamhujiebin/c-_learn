289727091c3663eed83b3fdb78b4ec902b53fe58
jiebin.hu <458249864@qq.com>
2020-04-18 23:38:52 +0800

    c++ 语法重温了 1.宏定义 2.信号量 3.线程创建、阻塞等待 4.vector<int>，泛型+标准库 5.cgi：其实就是c++的web服务器（ps：现在谁会用c++的web服务器呢？）

--- c++_learn.cpp
+++ c++_learn.cpp
@@ -2,6 +2,11 @@
 #include "Class.h"
 #include "Vitual.h"
 #include "Stack.h"
+#include <csignal>
+#include <Windows.h>
+#define PI 3.14
+#define MKSTR( x ) #x
+#define CONCAT(x,y) x ## y//## 运算符用于连接两个令牌
 
 using namespace std;
 
@@ -19,6 +24,12 @@ extern void testTime();
 extern void output();
 extern void TestStruct();
 extern void tryThrowCatch(int, int);
+
+void signalHandler(int signum) {
+	cout << "signal is good play:" << signum << endl;
+	exit(signum);
+}
+
 
 int main()
 {
@@ -56,22 +67,48 @@ int main()
 	{
 		cerr << msg << endl;
 	}*/
-	try {
-		Stack<int>         intStack;  // int 类型的栈 
-		Stack<string> stringStack;    // string 类型的栈 
+	//try {
+	//	Stack<int>         intStack;  // int 类型的栈 
+	//	Stack<string> stringStack;    // string 类型的栈 
 
-		// 操作 int 类型的栈 
-		intStack.push(7);
-		cout << intStack.top() << endl;
+	//	// 操作 int 类型的栈 
+	//	intStack.push(7);
+	//	cout << intStack.top() << endl;
 
-		// 操作 string 类型的栈 
-		stringStack.push("hello");
-		cout << stringStack.top() << std::endl;
-		stringStack.pop();
-		stringStack.pop();
+	//	// 操作 string 类型的栈 
+	//	stringStack.push("hello");
+	//	cout << stringStack.top() << std::endl;
+	//	stringStack.pop();
+	//	stringStack.pop();
+	//}
+	//catch (exception const& ex) {
+	//	cerr << "Exception: " << ex.what() << endl;
+	//	return -1;
+	//}
+
+	cout << "Value of PI :" << PI << endl;
+	cout << MKSTR(love) << endl;
+	string jiebinmengyin = "llalal";
+	cout << CONCAT(jiebin, mengyin) << endl;
+	int xy = 100;
+	cout << CONCAT(x, y) << endl;
+
+	cout << "Value of __LINE__ : " << __LINE__ << endl;
+	cout << "Value of __FILE__ : " << __FILE__ << endl;
+	cout << "Value of __DATE__ : " << __DATE__ << endl;
+	cout << "Value of __TIME__ : " << __TIME__ << endl;
+
+	int i = 0;
+	while (true)
+	{
+		i++;
+		cout << "等待你CTRL+C 噢亲" << endl;
+		Sleep(1000);
+		if (i == 1) {
+			raise(SIGINT);
+		}
 	}
-	catch (exception const& ex) {
-		cerr << "Exception: " << ex.what() << endl;
-		return -1;
-	}
+	signal(SIGINT, signalHandler);
+
 }
+
